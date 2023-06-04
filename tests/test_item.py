3f0d52b155a9b0405e8258d39adbae9345296f7d
Nikita <romanovnikita90@gmail.com>
2023-06-04 23:12:37 +0500

    выполнил дз

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+item1 = Item("Смартфон", 20000, 10)
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+
+def test_pay_rate():
+    assert Item.pay_rate == 1
+
+def test_apply_discount():
+    Item.pay_rate = 1.5
+    item1.apply_discount()
+    assert item1.price == 30000
